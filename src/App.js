b5f6c7ea6949e9f15e320fbf22deb0f6d5b904d6
Koji Okajima <koji970124@gmail.com>
2021-05-25 10:03:57 -0700

    changed server url

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
       if(tokenRes.data){
         const userRes = await axios.get(
-          'http://localhost:8000/users',
+          `${process.env.REACT_APP_SERVER_URL}/users/`,
           {
             headers: {"x-auth-token": token}
           }
